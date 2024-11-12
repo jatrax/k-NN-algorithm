50a579d670051b957b4dcc0d4a1f8a7463ce2548
Emre Özperçin <37477570+jatrax@users.noreply.github.com>
2024-11-12 13:48:47 +0300

    Add files via upload

--- k-NN.py
+++ k-NN.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 ARR = []
 SIZE = 1000
-example_size = 3
+K = 5
 
 class Nouns:
     def __init__(self,x,y,type):
@@ -20,14 +20,28 @@ class Nouns:
             canvas.create_rectangle(x-5, y-5, x+5 , y+5 , fill="red")
         ARR.append([x,y,type])
 
+def normalize(x,max,min):
+    return (x-min)/(max-min)
+
+import math
 def new_node(x,y):
     values = []
+    x_max = max([val[0] for val in ARR])
+    x_min = min([val[0] for val in ARR])
+    y_max = max([val[1] for val in ARR])
+    y_min = min([val[1] for val in ARR])
+    print(x_min)
     for i in ARR:
-        range_to_data = (i[0] - x)*(i[0] - x)+(i[1] - y)*(i[1] - y)
+        x0 = normalize(i[0],x_max,x_min)
+        x1 = normalize(x,x_max,x_min)
+        y0 = normalize(i[1],y_max,y_min)
+        y1 = normalize(y,y_max,y_min)
+        range_to_data = math.pow(x0-x1,2) + math.pow(y0-y1,2) #karelerinin toplamını al
+        range_to_data = math.sqrt(range_to_data) #kökünü al
         values.append([range_to_data,i[2],i[0],i[1]])
     values.sort()
     tmp = 0
-    for i in range(0,example_size):
+    for i in range(0,K):
         if values[i][1] == 0:
             tmp -= 1
         elif values[i][1] == 1:
@@ -70,6 +84,6 @@ Nouns(240,680,1)
 Nouns(155,550,1)
 Nouns(15,580,1)
 
-new_node(777,555)
+new_node(600,500)
 
 root.mainloop()
